a129bbc26e4794413b2815a182f2ad37d1d9ce94
tomriddle7 <tomriddle7@naver.com>
2020-09-17 16:29:49 +0900

    Updates

---
+++ precache-manifest.8b01c37063c4acc576a1955cc9228573.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec402bd3147e2340b32c7079cf5e64ba",
+    "url": "/nikipedia/index.html"
+  },
+  {
+    "revision": "09a0528ad928dfe69f71",
+    "url": "/nikipedia/static/js/2.c37f9186.chunk.js"
+  },
+  {
+    "revision": "620bbd8187d48f8f7d58c18ced9d4cc5",
+    "url": "/nikipedia/static/js/2.c37f9186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8857f2f9ed2976518d9",
+    "url": "/nikipedia/static/js/main.623b8cea.chunk.js"
+  },
+  {
+    "revision": "a7ed87b102d7e2e6b642",
+    "url": "/nikipedia/static/js/runtime-main.b994f828.js"
+  },
+  {
+    "revision": "5c4dc33699f63fa92f087a8aa3456969",
+    "url": "/nikipedia/static/media/m.5c4dc336.svg"
+  }
+]);
