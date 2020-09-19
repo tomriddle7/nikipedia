34cb684c32bd457b773b41812616f8a619396205
tomriddle7 <tomriddle7@naver.com>
2020-09-19 22:13:13 +0900

    Clean Code

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,42 +1,8 @@
 import React, { useState } from "react";
 import { authService, firebaseInstance } from "fbase";
+import AuthForm from "components/AuthForm";
 
 const Auth = (props) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [newAccount, setNewAccount] = useState(true);
-  const [error, setError] = useState("");
-  const onChange = (event) => {
-    const {
-      target: { name, value },
-    } = event;
-    if (name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
-    }
-  };
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      let data;
-      if (newAccount) {
-        data = await authService.createUserWithEmailAndPassword(email, password)
-        .then(user => {
-          authService.currentUser.updateProfile({
-            displayName: email.replace(/\@.*/gi, '')
-          });
-        });
-      } else {
-        data = await authService.signInWithEmailAndPassword(email, password);
-      }
-      props.history.goBack();
-      // console.log(data);
-    } catch (error) {
-      setError(error.message);
-    }
-  };
-  const toggleAccount = () => setNewAccount((prev) => !prev);
   const onSocialClick = async (event) => {
     const {
       target: { name },
@@ -53,32 +19,7 @@ const Auth = (props) => {
   };
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          name="email"
-          type="email"
-          placeholder="Email"
-          required
-          value={email}
-          onChange={onChange}
-        />
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          required
-          value={password}
-          onChange={onChange}
-        />
-        <input
-          type="submit"
-          value={newAccount ? "Create Account" : "Sign In"}
-        />
-        {error}
-      </form>
-      <span onClick={toggleAccount}>
-        {newAccount ? "Sign In" : "Create Account"}
-      </span>
+      <AuthForm {...props}/>
       <div>
         <button onClick={onSocialClick} name="google">
           Continue with Google
